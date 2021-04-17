b20e0671905dd9b35c850d7336716995883a61a8
Andrew Mascillaro <amascillaro@gmail.com>
2021-04-17 17:06:51 -0400

    Edit train dataset

--- data/split_data.py
+++ data/split_data.py
@@ -12,11 +12,17 @@ no_chess_df_test = no_chess_df.loc[no_chess_df.index.difference(no_chess_df_trai
 chess_only_df_train = chess_df.sample(frac=0.8)
 chess_only_df_test = chess_df.loc[chess_df.index.difference(chess_only_df_train.index)]
 
-no_chess_df_train.to_csv("no_chess_train.csv")
-no_chess_df_test.to_csv("no_chess_test.csv")
+# no_chess_df_train.to_csv("no_chess_train.csv", index=False)
+# no_chess_df_test.to_csv("no_chess_test.csv", index=False)
 
 chess_df_train = pd.concat([chess_only_df_train, no_chess_df_train])
-chess_df_train.to_csv("chess_train.csv")
+# chess_df_train.to_csv("chess_train.csv", index=False)
 
 chess_df_test = pd.concat([chess_only_df_test, no_chess_df_test])
-chess_df_test.to_csv("chess_test.csv")
+# chess_df_test.to_csv("chess_test.csv", index=False)
+
+mini_df_train = no_chess_df.sample(n=10)
+mini_df_test = no_chess_df.sample(n=10)
+
+mini_df_train.to_csv("mini_train.csv", index=False)
+mini_df_test.to_csv("mini_test.csv", index=False)
