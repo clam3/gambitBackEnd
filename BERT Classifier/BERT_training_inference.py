3a2aea2041ea8a72e6aeab83d2aee85d44e6619c
Andrew Mascillaro <amascillaro@gmail.com>
2021-04-17 22:27:04 -0400

    Lint the bert training scripts for sanity

--- BERT Classifier/BERT_training_inference.py
+++ BERT Classifier/BERT_training_inference.py
@@ -1,141 +1,172 @@
-#Necessary imports
+# Necessary imports
 
-import transformers 
+import transformers
 import torch
+
 # import neptune
 # from api_config import project_name,proxies,api_token
-import glob 
+import glob
 from transformers import get_linear_schedule_with_warmup
 from transformers import BertTokenizer, AutoTokenizer
 from transformers import BertForSequenceClassification, AdamW, BertConfig
 import random
-from bert_codes.feature_generation import combine_features,return_dataloader
+from bert_codes.feature_generation import combine_features, return_dataloader
 from bert_codes.data_extractor import data_collector
+
 # from bert_codes.own_bert_models import *
 from bert_codes.utils import *
-from sklearn.metrics import accuracy_score,f1_score
+from sklearn.metrics import accuracy_score, f1_score
 from tqdm import tqdm
 from BERT_inference import *
 from torch.utils.tensorboard import SummaryWriter
 import os
 import random
 
-writer = SummaryWriter('runs/gambit_experiment_1')
+writer = SummaryWriter("runs/gambit_experiment_1")
 
 # If there's a GPU available...
-if torch.cuda.is_available():    
-    # Tell PyTorch to use the GPU.    
+if torch.cuda.is_available():
+    # Tell PyTorch to use the GPU.
     device = torch.device("cuda")
-    print('There are %d GPU(s) available.' % torch.cuda.device_count())
-    print('We will use the GPU:', torch.cuda.get_device_name(0))
+    print("There are %d GPU(s) available." % torch.cuda.device_count())
+    print("We will use the GPU:", torch.cuda.get_device_name(0))
 # If not...
 else:
-    print('No GPU available, using the CPU instead.')
+    print("No GPU available, using the CPU instead.")
     device = torch.device("cpu")
 
-# # # Set the gpu device 
+# # # Set the gpu device
 print("current gpu device", torch.cuda.current_device())
 torch.cuda.set_device(0)
-print("current gpu device",torch.cuda.current_device())
+print("current gpu device", torch.cuda.current_device())
 
 
 # Initialize neptune for logging the parameters and metrics
 # neptune.init(project_name,api_token=api_token,proxies=proxies)
 # neptune.set_project(project_name)
-  
+
 # The main function that does the training
 
-def train_model(params,best_val_fscore):
-    
+
+def train_model(params, best_val_fscore):
+
     # In case of english languages, translation is the origin data itself.
-    if(params['language']=='English'):
-        params['csv_file']='aggregated_data.csv'
-    
+    if params["language"] == "English":
+        params["csv_file"] = "aggregated_data.csv"
 
     # train_path=params['files']+'/train/'+params['csv_file']
     # val_path=params['files']+'/val/'+params['csv_file']
 
-    train_path=params['files']+params['csv_file']
-    val_path=params['files']+params['csv_file']
+    train_path = params["files"] + params["csv_file"]
+    val_path = params["files"] + params["csv_file"]
 
     print(train_path)
 
     # Load the training and validation datasets
-    train_files=glob.glob(train_path)
-    val_files=glob.glob(val_path)
-    
-    #Load the bert tokenizer
-    print('Loading BERT tokenizer...')
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-uncased") # BertTokenizer.from_pretrained(params['path_files'], do_lower_case=False)
-    df_train = pd.read_csv(params["train_data"]) # data_collector(train_files,params,True)
-    df_val = pd.read_csv(params["validate_data"]) # data_collector(val_files,params,False)
-    
+    train_files = glob.glob(train_path)
+    val_files = glob.glob(val_path)
+
+    # Load the bert tokenizer
+    print("Loading BERT tokenizer...")
+    tokenizer = AutoTokenizer.from_pretrained(
+        "bert-base-multilingual-uncased"
+    )  # BertTokenizer.from_pretrained(params['path_files'], do_lower_case=False)
+    df_train = pd.read_csv(
+        params["train_data"]
+    )  # data_collector(train_files,params,True)
+    df_val = pd.read_csv(
+        params["validate_data"]
+    )  # data_collector(val_files,params,False)
+
     # Get the comment texts and corresponding labels
-    if(params['csv_file']=='aggregated_data.csv'):
-        sentences_train = df_train["message"].values 
+    if params["csv_file"] == "aggregated_data.csv":
+        sentences_train = df_train["message"].values
         sentences_val = df_val["message"].values
-    # elif(params['csv_file']=='*_translated.csv'):       --don't have to worry about LM  
+    # elif(params['csv_file']=='*_translated.csv'):       --don't have to worry about LM
     #     sentences_train = df_train.translated.values
     #     sentences_val = df_val.translated.values
-    
+
     labels_train = df_train["hate_speech"].values
     labels_val = df_val["hate_speech"].values
-    label_counts=df_train["hate_speech"].value_counts()
+    label_counts = df_train["hate_speech"].value_counts()
     print(label_counts)
-    label_weights = [ (len(df_train))/label_counts[0],len(df_train)/label_counts[1] ]
+    label_weights = [(len(df_train)) / label_counts[0], len(df_train) / label_counts[1]]
     print(label_weights)
-    
+
     # Select the required bert model. Refer below for explanation of the parameter values.
-    model=BertForSequenceClassification.from_pretrained("bert-base-multilingual-uncased") # select_model(params['what_bert'],params['path_files'],params['weights'])
+    model = BertForSequenceClassification.from_pretrained(
+        "bert-base-multilingual-uncased"
+    )  # select_model(params['what_bert'],params['path_files'],params['weights'])
     # Tell pytorch to run this model on the GPU.
     model.cuda()
 
     # Do the required encoding using the bert tokenizer
-    input_train_ids,att_masks_train=combine_features(sentences_train,tokenizer,params['max_length'])
-    input_val_ids,att_masks_val=combine_features(sentences_val,tokenizer,params['max_length'])
+    input_train_ids, att_masks_train = combine_features(
+        sentences_train, tokenizer, params["max_length"]
+    )
+    input_val_ids, att_masks_val = combine_features(
+        sentences_val, tokenizer, params["max_length"]
+    )
 
     # Create dataloaders for both the train and validation datasets.
-    train_dataloader = return_dataloader(input_train_ids,labels_train,att_masks_train,batch_size=params['batch_size'],is_train=params['is_train'])
-    validation_dataloader=return_dataloader(input_val_ids,labels_val,att_masks_val,batch_size=params['batch_size'],is_train=False)
-    
+    train_dataloader = return_dataloader(
+        input_train_ids,
+        labels_train,
+        att_masks_train,
+        batch_size=params["batch_size"],
+        is_train=params["is_train"],
+    )
+    validation_dataloader = return_dataloader(
+        input_val_ids,
+        labels_val,
+        att_masks_val,
+        batch_size=params["batch_size"],
+        is_train=False,
+    )
+
     # Initialize AdamW optimizer.
-    optimizer = AdamW(model.parameters(),
-                  lr = params['learning_rate'], # args.learning_rate - default is 5e-5, our notebook had 2e-5
-                  eps = params['epsilon'] # args.adam_epsilon  - default is 1e-8.
-                )
-    
+    optimizer = AdamW(
+        model.parameters(),
+        lr=params[
+            "learning_rate"
+        ],  # args.learning_rate - default is 5e-5, our notebook had 2e-5
+        eps=params["epsilon"],  # args.adam_epsilon  - default is 1e-8.
+    )
+
     # Number of training epochs (authors recommend between 2 and 4)
     # Total number of training steps is number of batches * number of epochs.
-    total_steps = len(train_dataloader) * params['epochs']
+    total_steps = len(train_dataloader) * params["epochs"]
 
     # Create the learning rate scheduler.
-    scheduler = get_linear_schedule_with_warmup(optimizer, 
-                                                num_warmup_steps = int(total_steps/10), # Default value in run_glue.py
-                                                num_training_steps = total_steps)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=int(total_steps / 10),  # Default value in run_glue.py
+        num_training_steps=total_steps,
+    )
 
     # Set the seed value all over the place to make this reproducible.
-    fix_the_random(seed_val = params['random_seed'])
+    fix_the_random(seed_val=params["random_seed"])
     # Store the average loss after each epoch so we can plot them.
     loss_values = []
 
-    # Create a new experiment in neptune for this run. 
-    bert_model = params['path_files']
-    language  = params['language']
-    name_one=bert_model+"_"+language
-    if(params['logging']=='neptune'):
+    # Create a new experiment in neptune for this run.
+    bert_model = params["path_files"]
+    language = params["language"]
+    name_one = bert_model + "_" + language
+    if params["logging"] == "neptune":
         pass
         # neptune.create_experiment(name_one,params=params,send_hardware_metrics=False,run_monitoring_thread=False)
         # neptune.append_tag(bert_model)
         # neptune.append_tag(language)
-        
+
     # The best val fscore obtained till now, for the purpose of hyper parameter finetuning.
-    best_val_fscore=best_val_fscore
+    best_val_fscore = best_val_fscore
 
     # For each epoch...
-    for epoch_i in range(0, params['epochs']):
+    for epoch_i in range(0, params["epochs"]):
         print("")
-        print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, params['epochs']))
-        print('Training...')
+        print("======== Epoch {:} / {:} ========".format(epoch_i + 1, params["epochs"]))
+        print("Training...")
 
         # Measure how long the training epoch takes.
         t0 = time.time()
@@ -152,22 +183,24 @@ def train_model(params,best_val_fscore):
                 # Calculate elapsed time in minutes.
                 elapsed = format_time(time.time() - t0)
             # `batch` contains three pytorch tensors:
-            #   [0]: input ids 
+            #   [0]: input ids
             #   [1]: attention masks
-            #   [2]: labels 
+            #   [2]: labels
             b_input_ids = batch[0].to(device)
             b_input_mask = batch[1].to(device)
             b_labels = batch[2].to(device)
             # (source: https://stackoverflow.com/questions/48001598/why-do-we-need-to-call-zero-grad-in-pytorch)
-            model.zero_grad()        
+            model.zero_grad()
 
             # Get the model outputs for this batch.
-            outputs = model(b_input_ids, 
-                        token_type_ids=None, 
-                        attention_mask=b_input_mask, 
-                        labels=b_labels)
+            outputs = model(
+                b_input_ids,
+                token_type_ids=None,
+                attention_mask=b_input_mask,
+                labels=b_labels,
+            )
 
-            # The call to `model` always returns a tuple, so we need to pull the 
+            # The call to `model` always returns a tuple, so we need to pull the
             # loss value out of the tuple.
             loss = outputs[0]
             # if(params['logging']=='neptune'):
@@ -177,7 +210,7 @@ def train_model(params,best_val_fscore):
 
             # Accumulate the training loss over all of the batches so that we can
             # calculate the average loss at the end. `loss` is a Tensor containing a
-            # single value; the `.item()` function just returns the Python value 
+            # single value; the `.item()` function just returns the Python value
             # from the tensor.
             total_loss += loss.item()
 
@@ -198,38 +231,37 @@ def train_model(params,best_val_fscore):
 
         # if(params['logging']=='neptune'):
         #     neptune.log_metric('avg_train_loss',avg_train_loss)
-        
+
         writer.add_scalar("Loss/train", avg_train_loss, epoch_i)
 
         # Store the loss value for plotting the learning curve.
         loss_values.append(avg_train_loss)
 
         # Compute the metrics on the validation and test sets.
-        val_fscore,val_accuracy=Eval_phase(params,'val',model)        
-        test_fscore,test_accuracy=Eval_phase(params,'test',model)
+        val_fscore, val_accuracy = Eval_phase(params, "val", model)
+        test_fscore, test_accuracy = Eval_phase(params, "test", model)
 
-        #Report the final accuracy and fscore for this validation run.
-        # if(params['logging']=='neptune'):    
+        # Report the final accuracy and fscore for this validation run.
+        # if(params['logging']=='neptune'):
         #     neptune.log_metric('val_fscore',val_fscore)
         #     neptune.log_metric('val_acc',val_accuracy)
         #     neptune.log_metric('test_fscore',test_fscore)
         #     neptune.log_metric('test_accuracy',test_accuracy)
 
-        writer.add_scalar('fscore/val', val_fscore, epoch_i)
-        writer.add_scalar('fscore/test', test_fscore, epoch_i)
-        writer.add_scalar('Accuracy/val', val_accuracy, epoch_i) 
-        writer.add_scalar('Accuracy/test', test_accuracy, epoch_i)
-        
+        writer.add_scalar("fscore/val", val_fscore, epoch_i)
+        writer.add_scalar("fscore/test", test_fscore, epoch_i)
+        writer.add_scalar("Accuracy/val", val_accuracy, epoch_i)
+        writer.add_scalar("Accuracy/test", test_accuracy, epoch_i)
 
-        # Save the model only if the validation fscore improves. After all epochs, the best model is the final saved one. 
-        if(val_fscore > best_val_fscore):
-            print(val_fscore,best_val_fscore)
-            best_val_fscore=val_fscore
+        # Save the model only if the validation fscore improves. After all epochs, the best model is the final saved one.
+        if val_fscore > best_val_fscore:
+            print(val_fscore, best_val_fscore)
+            best_val_fscore = val_fscore
 
-            save_model(model,tokenizer,params)         
+            save_model(model, tokenizer, params)
 
     # if(params['logging']=='neptune'):
-    #     neptune.stop()    
+    #     neptune.stop()
     writer.flush()
     writer.close()
     del model
@@ -237,12 +269,12 @@ def train_model(params,best_val_fscore):
     return best_val_fscore, val_accuracy
 
 
-# Explanation of all the params used below. 
+# Explanation of all the params used below.
 
 # 'logging':where logging {'local','neptune'}
 # 'language': language {'Arabic', 'English','German','Indonesian','Italian','Polish','Portugese','Spanish','French'}
-# 'is_train': whether train dataset 
-# 'is_model':is model 
+# 'is_train': whether train dataset
+# 'is_model':is model
 # 'learning_rate':Adam parameter lr
 # 'files': Path to the dataset folder ( containing the train, val and test subfolders)
 # 'csv_file': The regex used by glob to load the datasets. {'*_full.csv','*_translated.csv'} for untranslated and translated datasets respectively
@@ -257,68 +289,76 @@ def train_model(params,best_val_fscore):
 # 'to_save': whether to save the model or not
 # 'weights': weights for binary classifier
 # 'what_bert': type of bert possible option {'normal','weighted'}
-# 'save_only_bert': if only bert (without classifier) should be used 
+# 'save_only_bert': if only bert (without classifier) should be used
 # 'max_length': maximum length for input tokenization
 # 'random_seed': seed value for reproducibility
 
-params={
-    'logging':'neptune',
-    'language':'English',
-    'is_train':True,
-    'is_model':True,
-    'learning_rate':2e-5,
-    'files':'../data/',
-    'train_data': '../data/chess_train.csv',
-    'test_data': '../data/chess_test.csv',
-    'validate_data': '../data/chess_validate.csv',
-    'csv_file':'aggregated_data.csv',
-    'samp_strategy':'stratified',
-    'epsilon':1e-8,
-    'path_files':'bert-base-multilingual-uncased',
-    'take_ratio':False,
-    'sample_ratio':16,
-    'how_train':'baseline',
-    'epochs':12,
-    'batch_size':12,
-    'to_save':True,
-    'weights':[1.0,1.0],
-    'what_bert':'normal',
-    'save_only_bert':False,
-    'max_length':128,
-    'random_seed':42
-
+params = {
+    "logging": "neptune",
+    "language": "English",
+    "is_train": True,
+    "is_model": True,
+    "learning_rate": 2e-5,
+    "files": "../data/",
+    "train_data": "../data/chess_train.csv",
+    "test_data": "../data/chess_test.csv",
+    "validate_data": "../data/chess_validate.csv",
+    "csv_file": "aggregated_data.csv",
+    "samp_strategy": "stratified",
+    "epsilon": 1e-8,
+    "path_files": "bert-base-multilingual-uncased",
+    "take_ratio": False,
+    "sample_ratio": 16,
+    "how_train": "baseline",
+    "epochs": 12,
+    "batch_size": 12,
+    "to_save": True,
+    "weights": [1.0, 1.0],
+    "what_bert": "normal",
+    "save_only_bert": False,
+    "max_length": 128,
+    "random_seed": 42,
 }
 
 
+if __name__ == "__main__":
 
-if __name__=='__main__':
-
-    lang_map={'Arabic':'ar','French':'fr','Portugese':'pt','Spanish':'es','English':'en','Indonesian':'id','Italian':'it','German':'de','Polish':'pl','Spanish':
-    'es','French':'fr'}
+    lang_map = {
+        "Arabic": "ar",
+        "French": "fr",
+        "Portugese": "pt",
+        "Spanish": "es",
+        "English": "en",
+        "Indonesian": "id",
+        "Italian": "it",
+        "German": "de",
+        "Polish": "pl",
+        "Spanish": "es",
+        "French": "fr",
+    }
     torch.cuda.set_device(0)
 
-    params['language']='English'
-    sample_ratio,take_ratio = (32,False)
-    count=0
-    params['take_ratio']=take_ratio
-    params['sample_ratio']=sample_ratio
-    best_val_fscore=0
+    params["language"] = "English"
+    sample_ratio, take_ratio = (32, False)
+    count = 0
+    params["take_ratio"] = take_ratio
+    params["sample_ratio"] = sample_ratio
+    best_val_fscore = 0
     lr = 2e-5
-    params['learning_rate']=lr
-    params['samp_strategy']="stratified"
-    params['random_seed']=random.choice(range(1000))
-    count+=1
-    _,best_val_fscore=train_model(params,best_val_fscore)
+    params["learning_rate"] = lr
+    params["samp_strategy"] = "stratified"
+    params["random_seed"] = random.choice(range(1000))
+    count += 1
+    _, best_val_fscore = train_model(params, best_val_fscore)
 
-
-        # best_val_fscore=00
-        # for lr in [2e-5,3e-5,5e-5]:
-        #     params['learning_rate']=lr
-        #     params['samp_strategy']='stratified'
-        #     for seed in [2018,2019,2020,2021,2022][:1]:
-        #         params['random_seed']=seed
-        #         _,best_val_fscore=train_model(params,best_val_fscore)
-        # print('============================')
-        # print('Model for Language',lang,'is trained')
-        # print('============================')
-        # 
+    # best_val_fscore=00
+    # for lr in [2e-5,3e-5,5e-5]:
+    #     params['learning_rate']=lr
+    #     params['samp_strategy']='stratified'
+    #     for seed in [2018,2019,2020,2021,2022][:1]:
+    #         params['random_seed']=seed
+    #         _,best_val_fscore=train_model(params,best_val_fscore)
+    # print('============================')
+    # print('Model for Language',lang,'is trained')
+    # print('============================')
+    #
--- BERT Classifier/bert_codes/feature_generation.py
+++ BERT Classifier/bert_codes/feature_generation.py
@@ -3,12 +3,13 @@ import transformers
 from keras.preprocessing.sequence import pad_sequences
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 import numpy as np
+
 batch_size = 8
 
 MAX_LEN = 512
 
 # Function to tokenize given sentences
-def custom_tokenize(sentences,tokenizer,max_length=512):
+def custom_tokenize(sentences, tokenizer, max_length=512):
     input_ids = []
     # For every sentence...
     for sent in sentences:
@@ -20,34 +21,35 @@ def custom_tokenize(sentences,tokenizer,max_length=512):
         try:
 
             encoded_sent = tokenizer.encode(
-                                sent,                      # Sentence to encode.
-                                add_special_tokens = True, # Add '[CLS]' and '[SEP]'
-                                max_length = max_length,
-                                # This function also supports truncation and conversion
-                                # to pytorch tensors, but we need to do padding, so we
-                                # can't use these features :( .
-                                #max_length = 128,          # Truncate all sentences.
-                                #return_tensors = 'pt',     # Return pytorch tensors.
-                           )
+                sent,  # Sentence to encode.
+                add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+                max_length=max_length,
+                # This function also supports truncation and conversion
+                # to pytorch tensors, but we need to do padding, so we
+                # can't use these features :( .
+                # max_length = 128,          # Truncate all sentences.
+                # return_tensors = 'pt',     # Return pytorch tensors.
+            )
 
             # Add the encoded sentence to the list.
-            
+
         except ValueError:
             encoded_sent = tokenizer.encode(
-                                ' ',                      # Sentence to encode.
-                                add_special_tokens = True, # Add '[CLS]' and '[SEP]'
-                                max_length = max_length,
-                                # This function also supports truncation and conversion
-                                # to pytorch tensors, but we need to do padding, so we
-                                # can't use these features :( .
-                                #max_length = 128,          # Truncate all sentences.
-                                #return_tensors = 'pt',     # Return pytorch tensors.
-                           )
-              ### decide what to later
-        
+                " ",  # Sentence to encode.
+                add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+                max_length=max_length,
+                # This function also supports truncation and conversion
+                # to pytorch tensors, but we need to do padding, so we
+                # can't use these features :( .
+                # max_length = 128,          # Truncate all sentences.
+                # return_tensors = 'pt',     # Return pytorch tensors.
+            )
+            ### decide what to later
+
         input_ids.append(encoded_sent)
 
     return input_ids
+
 
 # Create mask for the given inputs.
 def custom_att_masks(input_ids):
@@ -65,25 +67,27 @@ def custom_att_masks(input_ids):
         attention_masks.append(att_mask)
     return attention_masks
 
+
 # Truncate and Tokenize sentences, then pad them
-def combine_features(sentences,tokenizer,max_length=512):
-    input_ids=custom_tokenize(sentences,tokenizer,max_length)
-    input_ids = pad_sequences(input_ids, dtype="long", 
-                          value=0, truncating="post", padding="post")
+def combine_features(sentences, tokenizer, max_length=512):
+    input_ids = custom_tokenize(sentences, tokenizer, max_length)
+    input_ids = pad_sequences(
+        input_ids, dtype="long", value=0, truncating="post", padding="post"
+    )
     print(input_ids.shape)
-    att_masks=custom_att_masks(input_ids)
-    return input_ids,att_masks
+    att_masks = custom_att_masks(input_ids)
+    return input_ids, att_masks
+
 
 # Generate pytorch data loader with the given dataset.
-def return_dataloader(input_ids,labels,att_masks,batch_size=8,is_train=False):
+def return_dataloader(input_ids, labels, att_masks, batch_size=8, is_train=False):
     inputs = torch.tensor(input_ids)
-    labels = torch.tensor(labels,dtype=torch.long)
+    labels = torch.tensor(labels, dtype=torch.long)
     masks = torch.tensor(np.array(att_masks))
     data = TensorDataset(inputs, masks, labels)
-    if(is_train==False):
+    if is_train == False:
         sampler = SequentialSampler(data)
     else:
         sampler = RandomSampler(data)
     dataloader = DataLoader(data, sampler=sampler, batch_size=batch_size)
     return dataloader
-
