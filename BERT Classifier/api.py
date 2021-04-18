4f44618225e27a87c17ca681a4ac0dce4a6777f0
Andrew Mascillaro <amascillaro@gmail.com>
2021-04-18 11:30:41 -0400

    Create test script and api

---
+++ BERT Classifier/api.py
@@ -0,0 +1,147 @@
+from test_model import (
+	load_model,
+	get_prediction
+)
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import ssl
+import json
+import traceback
+
+# server config
+PORT = 3000
+SSL_KEY = "ssl/private.key"
+SSL_CERT = "ssl/ca_bundle.crt"
+
+config, tokenizer, model = load_model("chess/bert-base-multilingual-uncased_English_translated_baseline_32/")
+
+class ModelRequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def do_GET(self):
+        try:
+            if self.path == "/":
+                self.path = "/index.html"
+
+            if self.path[-5:] == ".html":
+                with open("." + self.path, "r") as document:
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/html")
+                    self.end_headers()
+                    self.wfile.write(document.read().encode("utf-8"))
+
+            elif self.path[-3:] == ".js":
+                with open("." + self.path, "r") as script:
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/javascript")
+                    self.end_headers()
+                    self.wfile.write(script.read().encode("utf-8"))
+
+            elif self.path[-4:] == ".css":
+                with open("." + self.path, "r") as stylesheet:
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/css")
+                    self.end_headers()
+                    self.wfile.write(stylesheet.read().encode("utf-8"))
+
+            elif self.path[-4:] == ".svg":
+                print(self.path)
+                with open("." + self.path, "rb") as img:
+                    print("here")
+                    self.send_response(200)
+                    self.send_header("Content-type", "image/svg+xml")
+                    self.end_headers()
+                    text = img.read()
+                    self.wfile.write(text)
+
+            else:
+                with open("." + self.path, "rb") as file:
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write(file.read())
+
+        except:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"404 not found")
+
+    def do_POST(self):
+        try:
+            if self.path == "/api/parse":
+                length = int(self.headers.get("content-length"))
+                request = json.loads(self.rfile.read(length))
+
+                if request["text"] == "error":
+                    self.send_response(400)
+                    self.end_headers()
+                    self.wfile.write(b"400 bad request")
+
+                elif request["text"] == "not hate speech":
+                    self.send_response(200)
+                    self.send_header("Content-type", "application/json")
+                    self.send_header("Access-Control-Allow-Origin", "*")
+                    self.send_header("Vary", "Accept-Encoding, Origin")
+                    self.end_headers()
+                    self.wfile.write(
+                        json.dumps(
+                            {"confidence": 0}
+                        ).encode("utf-8")
+                    )
+
+                elif request["text"] == "hate speech":
+                    self.send_response(200)
+                    self.send_header("Content-type", "application/json")
+                    self.send_header("Access-Control-Allow-Origin", "*")
+                    self.send_header("Vary", "Accept-Encoding, Origin")
+                    self.end_headers()
+                    self.wfile.write(
+                        json.dumps(
+                            {"confidence": 1}
+                        ).encode("utf-8")
+                    )
+
+                else:
+                    text = request["text"]
+                    verdict = get_prediction(config, tokenizer, model, text)
+
+                    self.send_response(200)
+                    self.send_header("Content-type", "application/json")
+                    self.send_header("Access-Control-Allow-Origin", "*")
+                    self.send_header("Vary", "Accept-Encoding, Origin")
+                    self.end_headers()
+                    self.wfile.write(json.dumps(
+                    	{"confidence": verdict}
+                    ).encode("utf-8"))
+                    
+
+            else:
+                self.send_response(404)
+                self.end_headers()
+                self.wfile.write(b"404 not found")
+
+        except Exception:
+            traceback.print_exc()
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(b"500 internal server error")
+
+    def do_OPTIONS(self):
+        self.send_response(204)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type")
+        self.send_header("Access-Control-Max-Age", "86400")
+        self.send_header("Vary", "Accept-Encoding, Origin")
+        self.end_headers()
+
+
+if __name__ == "__main__":
+    print("Initializing server")
+    httpd = HTTPServer(("0.0.0.0", PORT), ModelRequestHandler)
+    print("Wrapping SSL")
+    # httpd.socket = ssl.wrap_socket(
+    #     httpd.socket, keyfile=SSL_KEY, certfile=SSL_CERT, server_side=True
+    # )
+    print("Serving on port %d" % PORT)
+    httpd.serve_forever()
--- BERT Classifier/test_model.py
+++ BERT Classifier/test_model.py
@@ -1,9 +1,14 @@
+import torch
+import torch.nn.functional as F
+import pandas as pd
+import numpy as np
 from transformers import (
 	BertModel,
 	BertTokenizer,
 	BertConfig
 )
-import pandas as pd
+
+from sklearn.metrics import matthews_corrcoef
 
 def load_model(model_path="models_saved/bert-base-multilingual-uncased_English_translated_baseline_32/"):
 	config = BertConfig.from_pretrained(model_path)
@@ -17,14 +22,36 @@ def tokenize_inputs(tokenizer, inputs):
 def run_model(model, tokenized_inputs):
 	return model(**tokenized_inputs)
 
-if __name__ == "__main__":
-	config, tokenizer, model = load_model("no_chess/bert-base-multilingual-uncased_English_translated_baseline_32/")
+def read_output(output):
+	logits = torch.mean(output[0][0], 0) # .detach().cpu().numpy()
+	probs = torch.sigmoid(logits).detach().numpy()
+	idx = np.argmax(probs)
+	return probs[idx]
 
-	tokenized_inputs = tokenize_inputs(tokenizer, "hello my name is bob")
+
+def get_prediction(config, tokenizer, model, inputs):
+	tokenized_inputs = tokenize_inputs(tokenizer, inputs)
+	prob = read_output(run_model(model, tokenized_inputs))
+	return float(prob)
+
+if __name__ == "__main__":
+	test_df = pd.read_csv("../data/no_chess_test.csv")
+	test_df_hate = test_df.loc[test_df["hate_speech"] == 1, "message"].tolist()[45]
+	# print(test_df_hate)
+	
+	config, tokenizer, model = load_model("chess/bert-base-multilingual-uncased_English_translated_baseline_32/")
+
+	tokenized_inputs = tokenize_inputs(tokenizer, "I love you")
 	output = run_model(model, tokenized_inputs)
 
-	test_df = pd.read_csv("../data/no_chess_test.csv")
-	test_inputs = tokenize_inputs(tokenizer, test_df["message"].tolist())
-	test_labels = test_df["hate_speech"]
+	new_tokenized_input = tokenize_inputs(tokenizer, test_df_hate)
+	new_output = run_model(model, new_tokenized_input)
+	new_prob = read_output(new_output)
+	# test_inputs = tokenize_inputs(tokenizer, test_df["message"].tolist())
+	# test_labels = test_df["hate_speech"]
 
-	test_output = run_model(model, test_inputs)
+	# test_output = run_model(model, test_inputs)
+	prob = read_output(output)
+
+	print(f"not hate speech? {prob}")
+	print(f"hate speech? {new_prob}")
