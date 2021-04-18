6ee419d06e52b6042c6ab4085b462d3a6c5952a9
Andrew Mascillaro <amascillaro@gmail.com>
2021-04-18 09:00:00 -0400

    Add testing script

---
+++ BERT Classifier/test_model.py
@@ -0,0 +1,24 @@
+from transformers import (
+	BertModel,
+	BertTokenizer,
+	BertConfig
+)
+
+
+def load_model(model_path="models_saved/bert-base-multilingual-uncased_English_translated_baseline_32/"):
+	config = BertConfig.from_pretrained(model_path)
+	tokenizer = BertTokenizer.from_pretrained(model_path)
+	model = BertModel.from_pretrained(model_path)
+	return config, tokenizer, model
+
+def tokenize_inputs(tokenizer, inputs):
+	return tokenizer(inputs, return_tensors="pt")
+
+def run_model(model, tokenized_inputs):
+	return model(**tokenized_inputs)
+
+if __name__ == "__main__":
+	config, tokenizer, model = load_model("no_chess/bert-base-multilingual-uncased_English_translated_baseline_32/")
+
+	tokenized_inputs = tokenize_inputs("hello my name is bob")
+	output = run_model(model, tokenized_inputs)
